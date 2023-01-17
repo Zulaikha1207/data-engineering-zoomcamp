ea960183b0262c3bae4bbf055b60b3107994c6ef
Zulaikha1207 <zulaikhageer@gmail.com>
2023-01-17 21:04:21 +0100

    intoduction to docker

---
+++ week1_basics_n_setup/2_docker_sql/pipeline.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# pretend like there's some complex transformation happening here
+
+print(f"Job finished for day = {day}! Success!!")
